3c816c2ad941e5c4ac64c68174558d93c1eee77b
john Doe <your.email@example.com>
2026-01-30 21:36:11 +0300

    fix

--- app/lib/use_service_account.py
+++ app/lib/use_service_account.py
@@ -10,18 +10,17 @@ load_dotenv()
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # Load JSON key from env variable
-# service_account_info = json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON"))
+service_account_info = json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON"))
 
 # Load JSON key from file 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-json_path = os.path.join(script_dir, 'credential.json')
-with open(json_path, 'r') as file:
-    service_account_info = json.load(file)
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# json_path = os.path.join(script_dir, 'credential.json')
+# with open(json_path, 'r') as file:
+#     service_account_info = json.load(file)
 
 creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
 
 client = gspread.authorize(creds)
-#sheet = client.open_by_key(os.getenv("SPREADSHEET_ID")).sheet1
 
 
 # Fetching data from sheet1
